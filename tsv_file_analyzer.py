c4b12631cf6b4c2917cd5892eb8b124a3d0e5906
Jaideep Sundaram <jai.python3@gmail.com>
2020-04-06 18:32:11 -0400

    ADDS support for only displaying the columns names and their position

--- tsv_file_analyzer.py
+++ tsv_file_analyzer.py
@@ -12,13 +12,28 @@ from colorama import Fore, Style
 from datetime import datetime
 from datetime import date
 
+DEFAULT_COLUMNS_ONLY = False
+
 DEFAULT_OUTDIR = "/tmp/" + os.path.basename(__file__) + '/' + str(datetime.today().strftime('%Y-%m-%d-%H%M%S'))
 
 LOGGING_FORMAT = "%(levelname)s : %(asctime)s : %(pathname)s : %(lineno)d : %(message)s"
 
 LOG_LEVEL = logging.INFO
 
+
 position_to_header_lookup = {}
+
+
+def display_columns(parts):
+
+    print(Fore.YELLOW + "\nHere are the columns\n")
+    print(Style.RESET_ALL + '', end='')
+
+    for i, field in enumerate(parts, 1):
+        field = field.replace('"', '')      
+        print("{}. ".format(i), end='')  
+        print(Fore.BLUE + "{}".format(field))
+        print(Style.RESET_ALL + '', end='')
 
 
 def display_record(parts, rec_ctr, line_ctr):
@@ -40,7 +55,8 @@ def display_record(parts, rec_ctr, line_ctr):
 @click.option('--outdir', help='The default is the current working directory')
 @click.option('--infile', help="The tab-delimited file to be analyzed")
 @click.option('--logfile', help="The log file")
-def main(outdir, infile, logfile):
+@click.option('--columns_only', is_flag=True, help="If specified, will only display the column names")
+def main(outdir, infile, logfile, columns_only):
     """Analyze a tab-delimited file
     """
 
@@ -75,6 +91,11 @@ def main(outdir, infile, logfile):
 
     assert isinstance(logfile, str)
 
+    if columns_only is None:
+        columns_only = DEFAULT_COLUMNS_ONLY
+        print(Fore.YELLOW + "--columns_only was not specified and therefore was set to '{}'".format(columns_only))
+        print(Style.RESET_ALL + '', end='')
+
     logging.basicConfig(filename=logfile, format=LOGGING_FORMAT, level=LOG_LEVEL)
 
     global position_to_header_lookup
@@ -90,6 +111,9 @@ def main(outdir, infile, logfile):
                 for i, header in enumerate(parts):
                     header = header.replace('"', '')
                     position_to_header_lookup[i] = header.strip()
+                if columns_only:
+                    display_columns(parts)
+                    break
                 continue
             else:
                 rec_ctr += 1
