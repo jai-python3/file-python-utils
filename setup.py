d8940fcde739a752a6cfe93496a4952b8ce83708
Jaideep Sundaram <jai.python3@gmail.com>
2023-12-30 20:25:48 -0500

    Bump version: 0.3.1 → 0.4.0

--- setup.py
+++ setup.py
@@ -54,6 +54,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/jai-python3/data-file-utils',
-    version='0.3.1',
+    version='0.4.0',
     zip_safe=False,
 )
